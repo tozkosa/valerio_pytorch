3747415f88488d7948adeca2846675e8f7935478
Tomoko Ozeki <tomoko.ozeki@gmail.com>
2021-09-01 18:27:45 +0900

    treating dataframe

--- cwt_prac.py
+++ cwt_prac.py
@@ -6,7 +6,7 @@ import librosa
 import os
 
 
-def load_audio_files(place, train_test):
+def load_audio_files(place, train_test, hammer='small'):
     df = pd.read_csv("annotations.csv")
     df2 = df[df['place'] == place]
     df3 = df2[df2['train_test'] == train_test]
@@ -15,19 +15,23 @@ def load_audio_files(place, train_test):
     df3.to_csv('temp.csv')
 
     X = []
-    for i, (path, file_name) in enumerate(zip(df3['path'], df3['file_name'])):
-        file_path = os.path.join(path, file_name)
-        signal, sr = librosa.load(file_path)
-        print(f"{i}: {file_name}, sr={sr}, n_frames={len(signal)}")
-        X.append(signal)
+    for i, (path, file_name, h_t) in enumerate(zip(df3['path'],
+                                                   df3['file_name'],
+                                                   df3['hammer_type'])):
+        if h_t == hammer:
+            file_path = os.path.join(path, file_name)
+            signal, sr = librosa.load(file_path)
+            print(f"{i}: {file_name}, sr={sr}, n_frames={len(signal)}")
+            X.append(signal)
+            print(file_name)
 
     return X, sr
 
 
 if __name__ == "__main__":
-    X, sr = load_audio_files('crack_1', 'test')
+    X, sr = load_audio_files('wall_member_50cm_thick', 'train', 'small')
     wavlist = pywt.wavelist(kind='continuous')
-    print(wavlist)
+    # print(wavlist)
     wavelet_type = 'cmor1.5-1.0'  # 'cmor1.5-1.0'
     # wav = pywt.ContinuousWavelet(wavelet_type)
     # int_psi, x = pywt.integrate_wavelet(wav, precision=8)
@@ -36,29 +40,35 @@ if __name__ == "__main__":
     # print(len(int_psi))
     fs = sr
     nq_f = fs / 2.0
-    print(fs)
-    print(nq_f)
+    # print(fs)
+    # print(nq_f)
 
     freqs = np.linspace(1, nq_f, 50)
     freqs_rate = freqs / fs
     scales = 1 / freqs_rate
     scales = scales[::-1]
-    print(len(scales))
+    # print(len(scales))
 
     frequencies_rate = pywt.scale2frequency(scale=scales, wavelet=wavelet_type)
-    print(frequencies_rate)
+    # print(frequencies_rate)
 
     frequencies = frequencies_rate * fs
-    print(frequencies)
+    # print(frequencies)
 
-    for i in range(0, 64, 4):
+    if len(X) > 20:
+        num = 20
+    else:
+        num = len(X)
+
+    for i in range(0, num, 4):
         signal = X[i]
         cwtmatr, freqs_rate = pywt.cwt(signal, scales=scales, wavelet=wavelet_type)
-        print(cwtmatr.shape)
+        # print(cwtmatr.shape)
         plt.figure()
         plt.imshow(np.abs(cwtmatr), aspect='auto')
 
     plt.show()
+    plt.close()
     # scale = np.arange(1, 1025) # 257
     # signal = X[2]
     # coef, _ = pywt.cwt(signal, scales=scale, wavelet=wavelet_type)
