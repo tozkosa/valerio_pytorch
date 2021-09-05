0d6b2ebafccb42a68ea8cebc112b43ff961e8b91
Tomoko Ozeki <tomoko.ozeki@gmail.com>
2021-09-05 13:48:27 +0900

    on linux

--- make_annotations.py
+++ make_annotations.py
@@ -1,13 +1,14 @@
 import os
 import pandas as pd
 
-DATA_ROOT = "D:\daon_data\\nagoya_20210727_cutout_5ms"
-# DATA_ROOT = "/home/tozeki/daon/nagoya2021/nagoya_20210727_cutout_5ms"
+# DATA_ROOT = "D:\daon_data\\nagoya_20210727_cutout_5ms"
+DATA_ROOT = "/home/tomoko/daon/nagoya2021/nagoya_20210727_cutout_5ms/"
 
 
 def list_of_dirs(data_root):
     print("inside list of dirs")
     print(data_root)
+    print(os.listdir(data_root))
     list1 = []
     list2 = []
     list3 = []
@@ -22,12 +23,12 @@ def list_of_dirs(data_root):
         if len(files) != 0:
             for file in files:
                 if file.endswith(".wav"):
-                    dir_split = curDir.split("\\")
+                    dir_split = curDir.split("/")
                     list1.append(curDir)
                     list2.append(file)
-                    list3.append(dir_split[3])
-                    list4.append(dir_split[4])
-                    list5.append(dir_split[5])
+                    list3.append(dir_split[6])
+                    list4.append(dir_split[7])
+                    list5.append(dir_split[8])
                     temp = dir_split[-1]
                     if (temp == 'defect') or (temp == 'normal'):
                         list6.append(temp)
@@ -41,9 +42,9 @@ def list_of_dirs(data_root):
     data = {'path': list1, 'file_name': list2, 'place': list3, 'train_test': list4,
             'hammer_type': list5, 'label': list6}
     df = pd.DataFrame(data)
-    df.to_csv('annotations.csv', encoding='sjis')
-    df2 = df[df['place'] == 'crack_1']
-    df2.to_csv('crack_1.csv')
+    df.to_csv('annotations_linux.csv', encoding='utf-8')
+    # df2 = df[df['place'] == 'crack_1']
+    # df2.to_csv('crack_1.csv')
 
 
 if __name__ == "__main__":
