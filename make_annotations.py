fb5331b80f34d0fe7d7167bc5babcb488a59f14c
Tomoko Ozeki <tomoko.ozeki@gmail.com>
2021-09-07 18:00:35 +0900

    fix filter in pandas

--- make_annotations.py
+++ make_annotations.py
@@ -2,8 +2,8 @@ import os
 import pandas as pd
 
 # DATA_ROOT = "D:\daon_data\\nagoya_20210727_cutout_5ms"
-# DATA_ROOT = "/home/tomoko/daon/nagoya2021/nagoya_20210727_cutout_5ms/"
-DATA_ROOT = "/home/tozeki/daon/nagoya2021/nagoya_20210727_cutout_5ms/" # 大学PC
+DATA_ROOT = "/home/tomoko/daon/nagoya2021/nagoya_20210727_cutout_5ms/" # home linux
+# DATA_ROOT = "/home/tozeki/daon/nagoya2021/nagoya_20210727_cutout_5ms/" # 大学PC
 
 
 def list_of_dirs(data_root):
@@ -28,9 +28,9 @@ def list_of_dirs(data_root):
                     print(f"length: {len(dir_split)}")
                     list1.append(curDir)
                     list2.append(file)
-                    list3.append(dir_split[-4])
-                    list4.append(dir_split[-3])
-                    list5.append(dir_split[-2])
+                    list3.append(dir_split[6])
+                    list4.append(dir_split[7])
+                    list5.append(dir_split[8])
                     temp = dir_split[-1]
                     if (temp == 'defect') or (temp == 'normal'):
                         list6.append(temp)
@@ -44,7 +44,7 @@ def list_of_dirs(data_root):
     data = {'path': list1, 'file_name': list2, 'place': list3, 'train_test': list4,
             'hammer_type': list5, 'label': list6}
     df = pd.DataFrame(data)
-    df.to_csv('annotations_linux.csv', encoding='utf-8')
+    df.to_csv('../annotations_home_linux.csv', encoding='utf-8')
     # df2 = df[df['place'] == 'crack_1']
     # df2.to_csv('crack_1.csv')
 
--- pandas_prac.py
+++ pandas_prac.py
@@ -1,25 +1,56 @@
 import pandas as pd
 import os
 import torch
+import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 import torchaudio
 
 
+class MLP(nn.Module):
+    def __init__(self, num_input_neurons):
+        super(MLP, self).__init__()
+        self.num_input_neurons = num_input_neurons
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(num_input_neurons, 256),
+            nn.ReLU(),
+            nn.Linear(256, 256),
+            nn.ReLU(),
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        y = self.linear_relu_stack(x)
+        return y
+
+
+def train(epoch, dataloader, model, loss_fn, optimizer):
+    size = len(dataloader)
+    # print(f"train_size: {size}")
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        pred = model(X)
+        # print(pred.shape)
+        # print(y.shape)
+        # print(pred)
+        # print(y)
+        loss = loss_fn(pred, y)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    print(f"Epoc: {epoch}, loss: {loss.item():>7f}")
+
+
 def make_annotation_train(place, train_test, hammer='small'):
-    df = pd.read_csv("annotations_linux.csv")
-    print(df)
-
-    df2 = df[df['place'] == place]
-    df3 = df2[df2['train_test'] == train_test]
-    df4 = df3[df3['hammer_type'] == hammer].reset_index()
-    df5 = df4[['path', 'file_name', 'label']]
-    print(f"df5: {df5}")
-
-    df5.to_csv('train.csv', encoding='utf-8')
-
-    # df3 = df2[df2['train_test'] == 'test']
-    # # df3 = df[['file_name', 'place', 'label']]
-    # print(df3.head(5))
+    df = pd.read_csv("../annotations_home_linux.csv")
+    df2 = df[(df['place'] == place) & (df['train_test'] == train_test) & (df['hammer_type'] == hammer)]
+    df3 = df2.reset_index()
+    df4 = df3[['path', 'file_name', 'label']]
+    df4.to_csv('../train_home.csv', encoding='utf-8')
 
 
 class ImpactEchoDataset(Dataset):
@@ -34,29 +65,45 @@ class ImpactEchoDataset(Dataset):
     # len(ied)
 
     def __getitem__(self, index):
-        print("inside getitem")
+        # print("inside getitem")
         audio_sample_path = self._get_audio_sample_path(index)
         label = self.annotations.iloc[index, -1]
+        label_dict = {'normal': torch.tensor([[0.]]), 'defect': torch.tensor([[1.]])}
         signal, sr = torchaudio.load(audio_sample_path)
-        return signal, label
+        return signal, label_dict[label]
 
     def _get_audio_sample_path(self, index):
         dir_name = self.annotations.iloc[index, 1]
-        print(dir_name)
+        # print(dir_name)
         file_name = self.annotations.iloc[index, 2]
         path = os.path.join(dir_name, file_name)
-        print(path)
+        # print(path)
         return path
 
 
 if __name__ == "__main__":
     make_annotation_train('crack_1', 'train')
-    training_data = ImpactEchoDataset('train.csv')
+    training_data = ImpactEchoDataset('../train_home.csv')
     print(f"There are {len(training_data)} samples in the dataset.")
-    signal, label = training_data[6]
-    print(signal)
+    signal, label = training_data[0]
+    input_neurons = signal.shape[1]
+    print(input_neurons)
+    print(label.dtype)
+    print(label.item())
 
-    a = 1
-    # train_dataloader = DataLoader(training_data, batch_size=16, shuffle=True)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(f"Using {device} device")
+
+    model = MLP(input_neurons).to(device)
+
+    train_dataloader = DataLoader(training_data, batch_size=8, shuffle=True)
+
+    loss = nn.BCEWithLogitsLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+    epochs = 1000
+    for t in range(epochs):
+        train(t, train_dataloader, model, loss, optimizer)
+
 
 
